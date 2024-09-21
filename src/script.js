e72df952bf7373115dd12064837a5b93c24ef162
Bhavesh Anandpara <bhavesh.anandpara@bentley.com>
2024-09-21 23:42:22 +0530

    orbit controls basics

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 
 // Variables
 const aspRatio = window.innerWidth/window.innerHeight
@@ -22,5 +23,24 @@ const canvas = document.querySelector('canvas.threejs')
 const renderer = new THREE.WebGLRenderer({canvas})
 renderer.setSize(window.innerWidth, window.innerHeight )
 
-renderer.render(scene, camera)
+// create controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.autoRotate = true
 
+/**
+ * renderloop - renders frames             
+ *              window.requestAnimationFrame() runs for every frame based on screen
+ *              So if screen has 60 fps it will run 60 times a sec.
+ *              
+ *              - we are passing renderloop as function which will execute every frame and render scene and camera  
+ */
+const renderloop= ()=>{
+
+    controls.update() //necessary for enableDamping & autoRotate
+
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(renderloop)
+}
+
+renderloop()
