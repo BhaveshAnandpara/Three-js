5e2d226aa1747fc8aa9cffa4c228f220b0c83302
Bhavesh Anandpara <bhavesh.anandpara@bentley.com>
2024-09-27 22:04:20 +0530

    animation added

--- src/script.js
+++ src/script.js
@@ -32,61 +32,11 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 // controls.autoRotate = true
 
-/*
- * ==================================================== Transform Objects ==============================================
- */
-
-// Copy method to copy Vector3 properties
-const newPos = new THREE.Vector3(0,1,0);
-cubeMesh.position.copy(newPos) // Copies positioN Values from newPos
-
-// distanceTo method finds the distance between 2 Vector3 Objects
-console.log(camera.position.distanceTo(cubeMesh.position)) // dist between camera and object
-
-// scale method 
-// cubeMesh.scale.set(2,2,2)  //set() helps to assign x,y,z value at same time
-// cubeMesh.scale.setScalar(2) //Same as set() only one value will be assigned to x,y,z
-
-// rotation
-// cubeMesh.rotation.y = Math.PI * 0.25 // PI/4 =>45 deg
-
-/*
-    Three js rotates x axis first then Y and then Z -> 'XYZ'
-    To force the three.js to rotate on Y Axis first and then X you will have to tell three.js
-    for that we use reorder()
- */
-cubeMesh.rotation.reorder('YXZ') 
-
-cubeMesh.rotation.y = THREE.MathUtils.degToRad(45) // PI/4 => 45 deg
-cubeMesh.rotation.x = THREE.MathUtils.degToRad(45) // PI/4 => 45 deg
-
-
-// Groups
-
-const cubeGrp = new THREE.Group();
-
-const cubeMesh1 = new THREE.Mesh(cubeGeometry, cubeMaterial)
-cubeMesh1.position.x = -2
-
-const cubeMesh2 = new THREE.Mesh(cubeGeometry, cubeMaterial)
-cubeMesh2.position.x = 2
-
-cubeGrp.add(cubeMesh)
-cubeGrp.add(cubeMesh1)
-cubeGrp.add(cubeMesh2)
-
-cubeGrp.scale.setScalar(1.5)
-cubeGrp.position.y = -1
-
-
-console.log(camera)
 
 // Add objects on screen
-// scene.add(cubeMesh) //comment this when using group
-scene.add(cubeGrp)
 scene.add(camera)
+scene.add(cubeMesh)
 scene.add(axesHelper)
-
 
 // resize according to window
 window.addEventListener('resize', ()=>{
@@ -96,7 +46,53 @@ window.addEventListener('resize', ()=>{
     renderer.setSize(window.innerWidth, window.innerHeight )
 })
 
+// ======================================================== Animations ========================================================
+const clock = new THREE.Clock() 
+let prevTime = 0
+
 const renderloop= ()=>{
+
+    const currentTime = clock.getElapsedTime(); // gets the exact time since the function fired
+    const delta = currentTime - prevTime // will get the time since last frame
+    prevTime = currentTime
+
+    // cubeMesh.rotation.x += 0.01 // This will rotate the cube but for devices with diff fps the rotation speed will change
+    /**
+     * delta - time between current frame and last frame
+     *         this makes sures rotation speed is consistent for all fps devices
+     *         let's say for device 1 - 1 fps, device 2 - 2 fps
+     *         that means for device 1 - renderloop will run each sec and same for device 2 but it will run every 2 sec
+     * 
+     *         if we are incrementing += 1 to rotation for a min 
+     *         device 1 -> 1*60 = 60 (loops runs for 60 times) 
+     *         device 2 -> 1*30 = 30 (loops runs for 30 times)
+     * 
+     *         to make sure both run same rotations
+     *         delta1 = 1, delta2 = 2
+     *          
+     *         if we are incrementing += 1*delta to rotation for a min
+     *         device 1 -> 1*1*60 = 60 (loops runs for 60 times)
+     *         device 2 -> 1*2*30 = 60 (loops runs for 30 times)
+     *          
+     */
+    // cubeMesh.rotation.x +=  THREE.MathUtils.degToRad(1) * delta * 40
+    // cubeMesh.rotation.y +=  THREE.MathUtils.degToRad(1) * delta * 40
+
+    /**
+     * 
+     * sin() and cos() gives value in range of -1 to 1
+     * 
+     * sin() - starts from 0
+     * cos() - starts from 1
+     * 
+     * sin() on x and cos() on y gives circle rotation`
+     * 
+     */
+
+    // cubeMesh.position.x = Math.sin(currentTime) * 5
+    // cubeMesh.position.y = Math.cos(currentTime) * 5
+    cubeMesh.scale.z = Math.tan(currentTime)
+
 
     controls.update() //necessary for enableDamping & autoRotate
 
