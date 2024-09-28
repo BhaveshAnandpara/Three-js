5c19388eedf297f4fb52f6c7a15e1486416d0334
Bhavesh Anandpara <bhavesh.anandpara@bentley.com>
2024-09-29 01:53:37 +0530

    intial commit

--- src/script.js
+++ src/script.js
@@ -10,11 +10,7 @@ const pane = new Pane();
 const scene = new THREE.Scene();
 
 // create a mesh(object)
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const planeGeometry = new THREE.PlaneGeometry(1, 1);
-const torusKnotGeometry = new THREE.TorusKnotGeometry();
 const sphereGeometry = new THREE.SphereGeometry(0.5, 52, 52);
-const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1);
 
 // TextureLoader
 const loader = new THREE.TextureLoader();
@@ -28,89 +24,42 @@ const textureMetallic = loader.load('/texture/whispy-grass-meadow-bl/wispy-grass
 const textureRoughness = loader.load('/texture/whispy-grass-meadow-bl/wispy-grass-meadow_roughness.png')
 const textureNormal = loader.load('/texture/whispy-grass-meadow-bl/wispy-grass-meadow_normal.png')
 
-/**
- * If wer are mapping texture on a 10x10 plane, the texture will be stretched 
- * Sol - repeat textures of 1x1 over 10x10  
- * wrapS - repeats horizontally (X-Axis)
- * wrapT - repeats vertically (Y-Axis)
- * ps: use space-ship-monitor-bl texture for repeat method
- */
-
-// texture.repeat.set(10,10)
-
-// for repearWrapping tile will be repeated from start 
-// 0-100,0-100,0-100.....
-// texture.wrapS = THREE.RepeatWrapping
-// texture.wrapT = THREE.RepeatWrapping
-
-// for MirroredRepeatWrapping tile will be mirrored and then repeated 
-// 0-100,100-0,0-100.....
-// texture.wrapS = THREE.MirroredRepeatWrapping
-// texture.wrapT = THREE.MirroredRepeatWrapping
-
-
-// pane.addBinding(texture, 'offset', {
-//     x:{
-//         min:-1,max:1,step:0.001
-//     },
-//     y:{
-//         min:-1,max:1,step:0.001
-//     }
-// })
 
 // Material
-
 const material = new THREE.MeshStandardMaterial();
-// material.side = THREE.DoubleSide
 material.map = texture
 material.aoMap = textureAo
 material.roughnessMap = textureRoughness
 material.roughness = 1
-material.metalnessMap = textureMetallic
 material.normalMap = textureNormal
 
-const cubeMesh = new THREE.Mesh(cubeGeometry, material);
-cubeMesh.position.y = -2;
-
-const planeMesh = new THREE.Mesh(planeGeometry, material);
-planeMesh.position.x = -2;
-// planeMesh.rotation.x = THREE.MathUtils.degToRad(90)
-// planeMesh.scale.setScalar(10)
-
-const torusKnotMesh = new THREE.Mesh(torusKnotGeometry, material);
-torusKnotMesh.position.x = 2;
-torusKnotMesh.scale.setScalar(0.4);
 
 const sphereMesh = new THREE.Mesh(sphereGeometry, material);
 
 
-const cylinderMesh = new THREE.Mesh(cylinderGeometry, material);
-cylinderMesh.position.y = 2;
+scene.add(sphereMesh);
 
-// scene.add(cubeMesh);
-scene.add(planeMesh);
-// scene.add(torusKnotMesh);
-// scene.add(sphereMesh);
-// scene.add(cylinderMesh);
-
-const rotate = new THREE.Group();
-rotate.add(cubeMesh, planeMesh, torusKnotMesh, sphereMesh, cylinderMesh);
-// rotate.add( planeMesh);
-scene.add(rotate)
 
 // Light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 10);
-pointLight.position.set(0, 0, 3);
+const pointLight = new THREE.PointLight(0xffffff,  1);
+pointLight.position.set(-1,-1,-1);
 
-// scene.add(pointLight);
-// scene.background = new THREE.Color('white')
+pane.addBinding(pointLight, 'position', {
+    x:{min:-10,max:10,step:0.1},
+    y:{min:-10,max:10,step:0.1},
+    z:{min:-10,max:10,step:0.1}
+})
+
+scene.add(pointLight);
+scene.add( new THREE.PointLightHelper( pointLight, 1 ))
+scene.add( new THREE.AxesHelper())
 
 // intialize a camera
 const camera = new THREE.PerspectiveCamera(75, aspRatio, 0.1, 30);
-camera.position.z = 5;
+camera.position.z = 2;
 camera.position.y = 2;
 
 // intialize a renederer
@@ -133,9 +82,6 @@ window.addEventListener("resize", () => {
 
 const renderloop = () => {
 
-//   rotate.children.forEach(mesh => {
-//     if(mesh instanceof THREE.Mesh) mesh.rotation.y += 0.02
-//   });
   controls.update(); //necessary for enableDamping & autoRotate
 
   renderer.render(scene, camera);
